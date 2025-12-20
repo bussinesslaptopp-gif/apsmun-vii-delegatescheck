81d43fd2243952fbdfd72bd46fb004342b9db3d2
bussinesslaptopp-gif <bussinesslaptopp@gmail.com>
2025-12-20 21:05:44 +0500

    Delete src directory

--- src/app/host-team/dc/generate/page.tsx
+++
@@ -1,7 +0,0 @@
-
-'use client';
-import { HostTeamGeneratePage } from '@/components/host-team-generate-page';
-
-export default function DCGeneratePage() {
-  return <HostTeamGeneratePage department="DC" />;
-}
--- src/app/host-team/ec/page.tsx
+++
@@ -1,8 +0,0 @@
-
-'use client';
-
-import { HostTeamVerificationPage } from '@/components/host-team-verification-page';
-
-export default function ECVerificationPage() {
-  return <HostTeamVerificationPage department="EC" />;
-}
