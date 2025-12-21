c023f43f898d63f4e88bd1eee09d9711206caf48
bussinesslaptopp-gif <bussinesslaptopp@gmail.com>
2025-12-21 16:07:23 +0500

    Add files via upload

---
+++ src/app/host-team/dc/page.tsx
@@ -0,0 +1,8 @@
+
+'use client';
+
+import { HostTeamVerificationPage } from '@/components/host-team-verification-page';
+
+export default function DCVerificationPage() {
+  return <HostTeamVerificationPage department="DC" />;
+}
