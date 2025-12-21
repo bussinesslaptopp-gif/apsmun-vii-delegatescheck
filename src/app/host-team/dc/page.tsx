88499bc0c03e1ad1ea81742465e4e3b250cd572e
bussinesslaptopp-gif <bussinesslaptopp@gmail.com>
2025-12-21 17:39:33 +0500

    Delete src directory

--- src/app/host-team/dc/page.tsx
+++
@@ -1,8 +0,0 @@
-
-'use client';
-
-import { HostTeamVerificationPage } from '@/components/host-team-verification-page';
-
-export default function DCVerificationPage() {
-  return <HostTeamVerificationPage department="DC" />;
-}
